0b62a3c4e5ae61116acb72a2763ada670c3c8bd3
Kerry Buckley <kerryjbuckley@gmail.com>
2012-03-10 14:23:08 +0000

    Rename feature step for consistency

--- features/home_page.feature
+++ features/home_page.feature
@@ -1,6 +1,6 @@
 Feature: Home page
 
   Scenario: Proposals are shown on the home page
-    Given some proposals have been made
+    Given some proposals have been submitted
     When I visit the site
-    Then I should see the proposal titles
+    Then I should see the submitted proposal titles
--- features/step_definitions/proposal_steps.rb
+++ features/step_definitions/proposal_steps.rb
@@ -1,11 +1,11 @@
-Given "some proposals have been made" do
+Given "some proposals have been submitted" do
 end
 
 Then "I should see some proposals" do
   page.should have_selector("section#proposals h2")
 end
 
-Then "I should see the proposal titles" do
+Then "I should see the submitted proposal titles" do
   conference.proposals.each do |proposal|
     page.should have_content(proposal.title)
   end
